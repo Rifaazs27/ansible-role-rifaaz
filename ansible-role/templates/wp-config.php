09be2112a7a1edfd92f5f28386474f43d3334139
Rifaazs27 <mougammadouzaafir@gmail.com>
2025-04-18 13:04:31 +0000

    Synchro 3

---
+++ ansible-role/templates/wp-config.php
@@ -0,0 +1,12 @@
+<?php
+/** La base de données WordPress */
+define( 'DB_NAME', '{{ wordpress_db_name }}' );
+
+/** Utilisateur de la base de données */
+define( 'DB_USER', '{{ wordpress_db_user }}' );
+
+/** Mot de passe de la base de données */
+define( 'DB_PASSWORD', '{{ wordpress_db_password }}' );
+
+/** Adresse de la base de données */
+define( 'DB_HOST', '{{ wordpress_db_host }}' );
